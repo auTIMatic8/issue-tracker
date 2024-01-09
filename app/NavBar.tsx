ac1fb7b3c9feb14a706a48475da60d7a0641cf1a
autimatic8 <autimatic808@gmail.com>
2024-01-09 15:18:32 -0800

    Create issue model with prisma

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -2,7 +2,7 @@
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import React from 'react'
-import { AiFillBug } from 'react-icons/ai'
+import { GiStarSwirl } from 'react-icons/gi'
 import classnames from 'classnames'
 
 const NavBar = () => {
@@ -14,7 +14,7 @@ const NavBar = () => {
   return (
     <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
       <Link href='/'>
-        <AiFillBug />
+        <GiStarSwirl />
       </Link>
       <ul className='flex space-x-6'>
         {links.map((link) => (
