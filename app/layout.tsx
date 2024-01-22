729885834f53c52b8ac80601ae6d59773f8f8c14
autimatic8 <autimatic808@gmail.com>
2024-01-22 13:22:47 -0800

    customize theme with interfont

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,12 @@
 import '@radix-ui/themes/styles.css'
+import './theme-config.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { Theme } from '@radix-ui/themes'
 import NavBar from './NavBar'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'], variable: '--font-inter' })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -20,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={inter.className}>
-        <Theme>
+        <Theme appearance='light' accentColor='cyan'>
           <NavBar />
           <main className='p-5'>{children}</main>
         </Theme>
